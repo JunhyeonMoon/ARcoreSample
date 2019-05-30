ac3cb2519e767a94f03d67f1c202c51a1e8d94d3
Junhyeon Moon <paranocs@gmail.com>
2019-05-31 02:09:13 +0900

    add makeLine

--- app/src/main/java/com/example/paranocs/arcoresample/ArActivity.java
+++ app/src/main/java/com/example/paranocs/arcoresample/ArActivity.java
@@ -3,13 +3,21 @@ package com.example.paranocs.arcoresample;
 import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import com.google.ar.core.Anchor;
+import com.google.ar.core.HitResult;
+import com.google.ar.core.Plane;
+import com.google.ar.sceneform.AnchorNode;
 import com.google.ar.sceneform.ArSceneView;
 import com.google.ar.sceneform.Node;
 import com.google.ar.sceneform.Scene;
 import com.google.ar.sceneform.collision.Sphere;
+import com.google.ar.sceneform.math.Quaternion;
 import com.google.ar.sceneform.math.Vector3;
 import com.google.ar.sceneform.rendering.Color;
 import com.google.ar.sceneform.rendering.Material;
@@ -18,6 +26,7 @@ import com.google.ar.sceneform.rendering.ModelRenderable;
 import com.google.ar.sceneform.rendering.Renderable;
 import com.google.ar.sceneform.rendering.ShapeFactory;
 import com.google.ar.sceneform.ux.ArFragment;
+import com.google.ar.sceneform.ux.BaseArFragment;
 import com.google.ar.sceneform.ux.TransformableNode;
 
 public class ArActivity extends AppCompatActivity {
@@ -25,10 +34,13 @@ public class ArActivity extends AppCompatActivity {
     private Context context;
 
     private ArFragment arFragment;
-    private ModelRenderable modelRenderable;
 
     private Button button_ball;
     private Button button_line;
+
+    private boolean lineActive = false;
+    private boolean isClickedSecond = false;
+    private Vector3 previousPoint;
 
 
     @Override
@@ -44,7 +56,7 @@ public class ArActivity extends AppCompatActivity {
         setButtonBallAction();
         setButtonLineAction();
 
-
+        makeLine();
     }
 
     private void setButtonBallAction(){
@@ -59,7 +71,13 @@ public class ArActivity extends AppCompatActivity {
         button_line.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                makeLine();
+                if(!lineActive) {
+                    lineActive = true;
+                    Toast.makeText(context, "plane을 클릭하면 선을 그립니다.", Toast.LENGTH_SHORT).show();
+                }else{
+                    lineActive = false;
+                    Toast.makeText(context, "선 그리기 비활성화", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
@@ -90,8 +108,58 @@ public class ArActivity extends AppCompatActivity {
         ball_node.setRenderable(ballRenderable[0]);
 
     }
+
     private void makeLine(){
-        //TODO make line
+        arFragment.setOnTapArPlaneListener(new BaseArFragment.OnTapArPlaneListener() {
+            @Override
+            public void onTapPlane(HitResult hitResult, Plane plane, MotionEvent motionEvent) {
+                if(!lineActive){
+                    return;
+                }
+
+                if(!isClickedSecond){
+                    isClickedSecond = true;
+                    previousPoint = new Vector3(hitResult.getHitPose().tx(),
+                            hitResult.getHitPose().ty(),
+                            hitResult.getHitPose().tz());
+                    Log.d(TAG, "previousPosition   " + previousPoint.x + ", " + previousPoint.y + ", " + previousPoint.z);
+                    Toast.makeText(context, "다음 지점을 선택해주세요", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                isClickedSecond = false;
+                Anchor anchor = hitResult.createAnchor();
+                AnchorNode anchorNode = new AnchorNode(anchor);
+                anchorNode.setParent(arFragment.getArSceneView().getScene());
+
+                Vector3 newPoint = anchorNode.getWorldPosition();
+                Log.d(TAG, "newPosition  " + newPoint.x + ", " + newPoint.y + ", " + newPoint.z);
+
+                Node node = new Node();
+
+                Vector3 distanceVec3 = Vector3.subtract(previousPoint, newPoint);
+                Vector3 directionVec3 = distanceVec3.normalized();
+                Quaternion rotation = Quaternion.lookRotation(directionVec3, Vector3.up());
+
+
+                Vector3 size = new Vector3(0.01f, 0.01f, distanceVec3.length());
+                Vector3 center = Vector3.add(previousPoint, newPoint).scaled(0.5f);
+
+                Color color_red = new Color(0xFFFF0000);
+                Renderable[] renderable = new Renderable[1];
+                MaterialFactory.makeOpaqueWithColor(context, color_red)
+                        .thenAccept(
+                                material -> {
+                                    renderable[0] = ShapeFactory.makeCube(size,
+                                            Vector3.zero(), material);
+                                });
+                node.setParent(anchorNode);
+                node.setRenderable(renderable[0]);
+                node.setWorldPosition(center);
+                node.setWorldRotation(rotation);
+            }
+        });
+
     }
 
 }
